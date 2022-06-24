beb899b81ce5500432e4e4851ada9d4df1d0e883
123poojakumari <108116509+123poojakumari@users.noreply.github.com>
2022-06-24 13:00:58 +0530

    Add files via upload

--- sum.java
+++ sum.java
@@ -2,12 +2,10 @@ import java.util.*;
 class sum {
 public static void main(String[] args) {
 Scanner sc = new Scanner(System.in);
-int n = sc.nextInt();
-
-for(int i = 1; i<=4; i++) {
-
-System.out.print("*");
-}
+System.out.println("enter the side of the square:");
+double a= sc.nextDouble();
+double perimeter=4*a;
+System.out.println("perimeter of squareis: " + perimeter);
 }
 }
 
