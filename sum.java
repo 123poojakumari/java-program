2f9fc02344cfbf4fe9db8642db7cc6cd41ca2210
123poojakumari <108116509+123poojakumari@users.noreply.github.com>
2022-06-24 12:33:20 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class sum {
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+int n = sc.nextInt();
+
+for(int i = 1; i<=4; i++) {
+
+System.out.print("*");
+}
+}
+}
+
